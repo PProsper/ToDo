98237e5dc13a0842f794d14964100655f83e560d
patrice prosper <new>
2017-01-12 16:02:09 -0500

    user can view a todo

--- src/App.js
+++ src/App.js
@@ -8,48 +8,71 @@ class App extends Component {
     this.state = { todos: {} };
 
     this.handleNewTodoInput = this.handleNewTodoInput.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
   }
 
-
-  createTodo(todoText) {
+  componentDidMount() {
     this.getTodos();
   }
-    getTodos() {
-      axios({
-        url: '/todos.json',
-        baseURL: 'https://your-todo-url.firebaseio.com/',
-        method: "GET"
-      }).then((response) => {
-        this.setState({ todos: response.data });
-      }).catch((error) => {
-        console.log(error);
-      });
-    }
 
-    createTodo(todoText) {
-      let newTodo = { title: todoText, createdAt: new Date };
+  getTodos() {
+    axios({
+      url: '/todos.json',
+      baseURL: 'https://to-do-ab08a.firebaseio.com',
+      method: "GET"
+    }).then((response) => {
+      this.setState({ todos: response.data });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
-      axios({
-        url: '/todos.json',
-        baseURL: 'https://your-todo-url.firebaseio.com/',
-        method: "POST",
-        data: newTodo
-      }).then((response) => {
-        let todos = this.state.todos;
-        let newTodoId = response.data.name;
-        todos[newTodoId] = newTodo;
-        this.setState({ todos: todos });
-      }).catch((error) => {
-        console.log(error);
-      });
-    }
-  
+  createTodo(todoText) {
+    let newTodo = ({ title: todoText, createdAt: new Date });
+
+    axios({
+      url: '/todos.json',
+      baseURL: 'https://to-do-ab08a.firebaseio.com',
+      method: "POST",
+      data: newTodo
+    }).then((response) => {
+      let todos = this.state.todos;
+      let newTodoId = response.data.name;
+      todos[newTodoId] = newTodo;
+      this.setState({ todos: todos });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  deleteTodo(todoId) {
+    axios({
+      url: `/todos/${todoId}.json`,
+      baseURL: 'https://to-do-ab08a.firebaseio.com',
+      method: "DELETE"
+    }).then((response) => {
+      let todos = this.state.todos;
+      delete todos[todoId];
+      this.setState({ todos: todos });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
   handleNewTodoInput(event) {
     if (event.charCode === 13) {
       this.createTodo(event.target.value);
       event.target.value = "";
     }
+  }
+
+
+  renderNewTodoBox() {
+    return (
+      <div className="new-todo-box pb-2">
+        <input className="w-100" placeholder="What do you have to do?" onKeyPress={ this.handleNewTodoInput } />
+      </div>
+    );
   }
 
   renderTodoList() {
@@ -60,10 +83,16 @@ class App extends Component {
 
       todoElements.push(
         <div className="todo d-flex justify-content-between pb-4" key={todoId}>
-          <div className="mt-2">
+          <div className="mt-2" onClick={ () => this.selectTodo(todoId) }>
             <h4>{todo.title}</h4>
             <div>{moment(todo.createdAt).calendar()}</div>
           </div>
+          <button
+            className="ml-4 btn btn-link"
+            onClick={ () => { this.deleteTodo(todoId) } }
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
       );
     }
@@ -75,26 +104,40 @@ class App extends Component {
     );
   }
 
-  renderNewTodoBox() {
+  selectTodo(todoId) {
+    this.setState({ currentTodo: todoId });
+  }
+
+  renderSelectedTodo() {
+    let content;
+
+    if (this.state.currentTodo) {
+      let currentTodo = this.state.todos[this.state.currentTodo];
+      content =  (
+        <div>
+          <h1>{currentTodo.title}</h1>
+        </div>
+      );
+    }
+
+    return content;
+  }
+
+  render() {
     return (
-      <div className="new-todo-box pb-2">
-        <input className="w-100" placeholder="What do you have to do?" onKeyPress={ this.handleNewTodoInput } />
+      <div className="App container-fluid">
+        <div className="row pt-3">
+          <div className="col-6 px-4">
+            {this.renderNewTodoBox()}
+            {this.renderTodoList()}
+          </div>
+          <div className="col-6 px-4">
+            {this.renderSelectedTodo()}
+          </div>
+        </div>
       </div>
     );
   }
-
-render() {
-  return (
-    <div className="App container-fluid">
-      <div className="row pt-3">
-        <div className="col-6 px-4">
-          {this.renderNewTodoBox()}
-          {this.renderTodoList()}
-        </div>
-      </div>
-    </div>
-  );
-}
 }
 
 export default App;
